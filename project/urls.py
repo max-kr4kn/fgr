cb56079eb85ff873512a0a450960ed9719656856
max-kr4kn <maxim.krchkn@gmail.com>
2017-10-10 15:13:14 +0300

    change Django version and update fixtures

--- project/urls.py
+++ project/urls.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from cms.sitemaps import CMSSitemap
 from django.conf import settings
-from django.conf.urls import include, url, patterns
+from django.conf.urls import include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
